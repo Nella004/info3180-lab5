0b4bcccb3eff39a6c1db9eadc42f868d4394563f
Jhanel Lalor <lalorjhanel4@gmail.com>
2025-04-13 21:33:57 -0500

    updates

--- src/views/AddMovieFormView.vue
+++ src/views/AddMovieFormView.vue
@@ -1,6 +1,11 @@
 <template>
-    <MovieForm />
-  </template>
+  <div>
+    <h1>Add Movie</h1>
+    <form id="movieForm" @submit.prevent="saveMovie">
+      <!-- Form fields for movie title, description, file upload, etc. -->
+    </form>
+  </div>
+</template>
   
   <script setup>
   import MovieForm from '@/components/MovieForm.vue';
