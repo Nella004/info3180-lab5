c052c048a724ced21ee7b68fa9eb8926e26775eb
Jhanel Lalor <lalorjhanel4@gmail.com>
2025-04-13 21:16:19 -0500

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from .config import Config
-from .models import db  
-
+from .models import db
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate  
+
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config.from_object(Config)
 
-db.init_app(app)  
+db.init_app(app)
+
+migrate = Migrate(app, db)  
 
 from app import views
