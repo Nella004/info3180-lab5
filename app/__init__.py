9fc01fd83472d49847c0db964593e676106417d7
Jhanel Lalor <lalorjhanel4@gmail.com>
2025-04-13 20:42:02 -0500

    Added movie model and migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from .config import Config
+from .models import db  
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db.init_app(app)  
 
 from app import views
